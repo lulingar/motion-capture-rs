c4aba45acbe060628398ff052c392b76cdb89460
Luis Linares <linares.luis@proton.me>
2024-05-01 17:27:10 +0200

    Change to Mpu9250, implement simple module for IMU tracking

---
+++ src/imu_tracker.rs
@@ -0,0 +1,56 @@
+use core::time::Duration;
+use std::time::Instant;
+use imu_fusion::{Fusion, FusionAhrsSettings, FusionEuler, FusionVector};
+
+pub struct ImuTracker {
+    pub sampling_period: Duration,
+    time: Instant,
+    pub euler: FusionEuler,
+    pub accel: FusionVector,
+    pub velocity: FusionVector,
+    pub position: FusionVector,
+    fusion: Fusion,
+    pub latest_delta: f32,
+}
+
+impl ImuTracker {
+    pub fn new(sampling_period: Duration, now: Instant) -> Self {
+        let sampling_freq: f32 = 1000_f32 / sampling_period.subsec_millis() as f32;
+        let ahrs_settings = FusionAhrsSettings::new();
+        let fusion = Fusion::new(sampling_freq as u32, ahrs_settings);
+        
+        Self {
+            sampling_period,
+            time: now,
+            euler: FusionEuler::zero(),
+            accel: FusionVector::zero(),
+            velocity: FusionVector::zero(),
+            position: FusionVector::zero(),
+            fusion,
+            latest_delta: 0f32
+        }
+    }
+    
+    pub fn update(&mut self, imu_accel: FusionVector, imu_gyro: FusionVector) {
+        // Gets: acceleration in units of earth gravity
+        //       angular rotation in degrees/sec
+        let now = Instant::now();
+        let delta = now.duration_since(self.time).as_secs_f32();
+        self.time = now;
+        self.latest_delta = delta;
+
+        //fusion.update_no_mag(gyro_vector, acceleration_vector, timestamp);
+        self.fusion.update_no_mag_by_duration_seconds(imu_gyro, imu_accel, delta);
+
+        // Gets heading in units of degrees
+        self.euler = self.fusion.euler();
+        self.accel = self.fusion.earth_acc();
+        self.velocity += self.accel * 9.81 * delta;
+        self.position += self.velocity * delta;
+    }
+    
+    pub fn latest_sampling_deviation(&self) -> f32 {
+        self.latest_delta / self.sampling_period.as_secs_f32()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
+mod imu_tracker;
+
 use core::time::Duration;
+use std::num::NonZeroU32;
 use std::time::Instant;
 use anyhow::Result;
 use esp_idf_svc::hal::{
     delay::FreeRtos,
-    gpio::PinDriver,
-    gpio::{Gpio20, Gpio21, Output},
+    gpio::{AnyOutputPin, PinDriver},
     i2c::{I2cConfig, I2cDriver},
     peripherals::Peripherals,
+    //sys::EspError,
     units::FromValueType,
 };
-use embedded_hal_bus::i2c;
-use core::cell::RefCell;
-use embedded_hal_bus::i2c::RefCellDevice;
-//use esp_idf_svc::sys::EspError;
+use esp_idf_svc::hal::spi::{SpiDeviceDriver, SpiDriverConfig};
+use esp_idf_svc::hal::spi::config::Config;
+use esp_idf_svc::hal::task::notification::Notification;
 use esp_idf_svc::timer::EspTaskTimerService;
-use mpu6050::{
-    Mpu6050,
-    Mpu6050Error,
-    device::{ AccelRange, GyroRange, ACCEL_HPF },
-    PI_180,
-};
-use imu_fusion::{Fusion, FusionAhrsSettings, FusionVector};
+use imu_fusion::FusionVector;
+use mpu9250::{ImuMeasurements, Mpu9250};
+use crate::imu_tracker::ImuTracker;
 
-//use rand::prelude::*;
 
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -34,109 +31,88 @@ fn main() -> Result<()> {
 
     log::info!("Hello, world!");
 
+    let mut delay = FreeRtos;
     let peripherals = Peripherals::take().unwrap();
 
     let i2c_config = I2cConfig::new().baudrate(400.kHz().into());
-    let sda_imu = peripherals.pins.gpio1;
-    let scl_imu = peripherals.pins.gpio0;
-    let i2c_imu = I2cDriver::new(peripherals.i2c0, sda_imu, scl_imu, &i2c_config)?;
-    let i2c_ref_cell = RefCell::new(i2c_imu);
-    
+    let sda = peripherals.pins.gpio1;
+    let scl = peripherals.pins.gpio0;
+    let _i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &i2c_config)?;
+
+    let sclk = peripherals.pins.gpio6;
+    let miso = peripherals.pins.gpio2;
+    let mosi = peripherals.pins.gpio7;
+    let cs = PinDriver::output(peripherals.pins.gpio10)?;
+    let spi_config = Config::default().baudrate(1.MHz().into());
+    let spi = SpiDeviceDriver::new_single(
+        peripherals.spi2,
+        sclk,
+        mosi,
+        Some(miso),
+        Option::<AnyOutputPin>::None,
+        &SpiDriverConfig::new(),
+        &spi_config
+    )?;
+    let mut imu = Mpu9250::imu_default(spi, cs, &mut delay).expect("Could not init IMU!");
+    let who_am_i = imu.who_am_i().expect("could not read WHO_AM_I");
+    println!("WHO_AM_I: 0x{:x}", who_am_i);
+
+    /*
+    let acc_offsets: [f32; 3] = if let Ok(offsets) = imu.calibrate_at_rest(&mut delay) {
+        offsets
+    } else {
+        log::warn!("Could not calibrate offsets!");
+        [0f32; 3]
+    };
+    println!("Calibration is {:?}", acc_offsets);
+    */
+
     let mut flag_total = PinDriver::output(peripherals.pins.gpio21)?;
     let mut flag_compute = PinDriver::output(peripherals.pins.gpio20)?;
     flag_total.set_low()?;
     flag_compute.set_low()?;
 
-    let mut delay = FreeRtos;
-    let mut mpu = Mpu6050::new(i2c::RefCellDevice::new(&i2c_ref_cell));
-
-    /* Supply chain issue:
-     * https://forum.arduino.cc/t/mpu-6050-a-module-problems-who-am-i-reports-0x98-not-0x68-as-it-should-fake-mpu-6050/861956/20
-     */
-    match mpu.init(&mut delay) {
-        Ok(_) => (),
-        Err(error) => match error {
-            Mpu6050Error::I2c(_) => log::error!("I2C initialization failed."),
-            Mpu6050Error::InvalidChipId(read) => log::warn!("Maybe counterfeit chip? GottenID {:#x}.", read),
-        }
-    };
-
-    mpu.set_accel_range(AccelRange::G4).unwrap();
-    mpu.set_gyro_range(GyroRange::D250).unwrap();
-    mpu.set_accel_hpf(ACCEL_HPF::_RESET).unwrap();
-
-    /*
-    loop {
-        let mut tic = Instant::now();
-        let toc = Instant::now();
-        //let dt = toc - tic;
-        tic = toc;
-    }
-    */
+    // Configures the notification
+    let notification = Notification::new();
+    let notifier = notification.notifier();
+    
     const IMU_SAMPLE_PERIOD: Duration = Duration::from_millis(5);
-    const IMU_SAMPLE_FREQ: f32 = 1000_f32 / IMU_SAMPLE_PERIOD.subsec_millis() as f32;
-    let ahrs_settings = FusionAhrsSettings::new();
-    let mut fusion = Fusion::new(IMU_SAMPLE_FREQ as u32, ahrs_settings);
-    let mut pos = FusionVector::zero();
-    let mut vel = FusionVector::zero();
-    let start_time = Instant::now();
+    let mut tracker = ImuTracker::new(IMU_SAMPLE_PERIOD, Instant::now());
 
     let timer_service = EspTaskTimerService::new().unwrap();
-    let callback_timer = timer_service.timer(move || acq_cmp(
-        &mut mpu,
-        &mut fusion,
-        start_time,
-        &IMU_SAMPLE_PERIOD,
-        &mut pos, &mut vel,
-        &mut flag_compute,
-        &mut flag_total,
-    ))?;
-    callback_timer.every(IMU_SAMPLE_PERIOD).unwrap();
+    let callback_timer = timer_service.timer(move || unsafe {
+        notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
+    })?;
+    callback_timer.every(IMU_SAMPLE_PERIOD)?;
 
+    const REPORT_PERIOD: Duration = Duration::from_millis(200);
+    const SKIP_COUNT: u32 = REPORT_PERIOD.as_millis() as u32 / (IMU_SAMPLE_PERIOD.as_millis() as u32);
+    let mut skip = 0;
+    
     loop {
-        FreeRtos::delay_ms(100);
+        notification.wait(esp_idf_svc::hal::delay::BLOCK);
+        
+        flag_total.set_high().unwrap();
+        flag_compute.set_high().unwrap();        
+        let all: ImuMeasurements<[f32;3]> = imu.all().expect("Unable to read from IMU!");
+        let imu_gyro = FusionVector::new(all.gyro[0], all.gyro[1],all.gyro[2]) * (180f32 / std::f32::consts::PI);
+        let imu_accel = FusionVector::new(all.accel[0],all.accel[1],all.accel[2]);
+        tracker.update(imu_accel, imu_gyro);
+        flag_compute.set_low().unwrap();
+
+        let deviation = 100f32 * (tracker.latest_sampling_deviation() - 1f32);
+        skip += 1;
+        if skip == SKIP_COUNT {
+            skip = 0;
+            //println!("... px:{:+.3}, py:{:+.3}, pz:{:+.3}",
+            //         pos_vector.x, pos_vector.y, pos_vector.z);
+            println!("dt: {:5.2}ms ({:.1}%), temp: {:.1}C, pitch:{:+.1} roll:{:+.1} yaw:{:+.1} ae_x:{:+.3} ae.y:{:+.3} ae.z:{:+.3}",
+                     1000f32*tracker.latest_delta, deviation,
+                     all.temp,
+                     tracker.euler.angle.pitch, tracker.euler.angle.roll, tracker.euler.angle.yaw,
+                     tracker.accel.x, tracker.accel.y, tracker.accel.z,
+            );
+        }
+        flag_total.set_low().unwrap();
     }
-}
-
-fn acq_cmp(
-    mpu: &mut Mpu6050<RefCellDevice<I2cDriver>>,
-    fusion: &mut Fusion,
-    start_time: Instant,
-    sampling_period: &Duration,
-    pos_vector: &mut FusionVector,
-    vel_vector: &mut FusionVector,
-    flag_compute: &mut PinDriver<Gpio20, Output>,
-    flag_total: &mut PinDriver<Gpio21, Output>,
-) {
-    flag_total.set_high().unwrap();
-    flag_compute.set_high().unwrap();
-    let ts = start_time.elapsed().as_secs_f32();
-
-    // Gets acceleration in units of earth gravity
-    let acc = mpu.get_acc().unwrap();
-    // Gets angular rotation in degrees/sec
-    let gyr = mpu.get_gyro().unwrap() / PI_180;
-
-    let gyro_vector = FusionVector::new(gyr.x, gyr.y, gyr.z);
-    let acceleration_vector = FusionVector::new(acc.x, acc.y, acc.z);
-    fusion.update_no_mag(gyro_vector, acceleration_vector, ts);
-    // Gets heading in units of degrees
-    let euler = fusion.euler();
-    let earth_acc = fusion.earth_acc();
-    *vel_vector += earth_acc * 9.81 * sampling_period.as_secs_f32();
-    *pos_vector += *vel_vector * sampling_period.as_secs_f32();
-    flag_compute.set_low().unwrap();
-
-    /*
-    println!("ts: {:5.5}, pitch:{:+.3} roll:{:+.3} yaw:{:+.3}, px:{:+.3}, py:{:+.3}, pz:{:+.3}",
-             ts,
-             euler.angle.pitch, euler.angle.roll, euler.angle.yaw,
-             pos_vector.x, pos_vector.y, pos_vector.z);
-    */
-    println!("ts: {:5.5}, pitch:{:+.3} roll:{:+.3} yaw:{:+.3} ae_x:{:+.3} ae.y:{:+.3} ae.z:{:+.3}",
-             ts,
-             euler.angle.pitch, euler.angle.roll, euler.angle.yaw,
-             earth_acc.x, earth_acc.y, earth_acc.z,
-    );
-    flag_total.set_low().unwrap();
 }
