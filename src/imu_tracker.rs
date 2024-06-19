5b09f1bfcbe5b51ee7201a65d2240ad36bb37334
Luis Linares <linares.luis@proton.me>
2024-06-19 19:21:48 +0200

    Reduce program and add raw sample processing notebook

--- src/imu_tracker.rs
+++ src/imu_tracker.rs
@@ -1,6 +1,6 @@
 use core::time::Duration;
-use std::time::Instant;
 use imu_fusion::{Fusion, FusionAhrsSettings, FusionEuler, FusionVector};
+use std::time::Instant;
 
 pub struct ImuTracker {
     pub sampling_period: Duration,
@@ -18,7 +18,7 @@ impl ImuTracker {
         let sampling_freq: f32 = 1000_f32 / sampling_period.subsec_millis() as f32;
         let ahrs_settings = FusionAhrsSettings::new();
         let fusion = Fusion::new(sampling_freq as u32, ahrs_settings);
-        
+
         Self {
             sampling_period,
             time: now,
@@ -27,10 +27,10 @@ impl ImuTracker {
             velocity: FusionVector::zero(),
             position: FusionVector::zero(),
             fusion,
-            latest_delta: 0f32
+            latest_delta: 0f32,
         }
     }
-    
+
     pub fn update(&mut self, imu_accel: FusionVector, imu_gyro: FusionVector) {
         // Gets: acceleration in units of earth gravity
         //       angular rotation in degrees/sec
@@ -40,7 +40,8 @@ impl ImuTracker {
         self.latest_delta = delta;
 
         //fusion.update_no_mag(gyro_vector, acceleration_vector, timestamp);
-        self.fusion.update_no_mag_by_duration_seconds(imu_gyro, imu_accel, delta);
+        self.fusion
+            .update_no_mag_by_duration_seconds(imu_gyro, imu_accel, delta);
 
         // Gets heading in units of degrees
         self.euler = self.fusion.euler();
@@ -48,9 +49,8 @@ impl ImuTracker {
         self.velocity += self.accel * 9.81 * delta;
         self.position += self.velocity * delta;
     }
-    
+
     pub fn latest_sampling_deviation(&self) -> f32 {
         self.latest_delta / self.sampling_period.as_secs_f32()
     }
 }
-
